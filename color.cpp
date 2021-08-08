71c40634a096630ceb9602b237315443fd10c25b
pierreneillo <75447740+pierreneillo@users.noreply.github.com>
2021-08-08 13:09:35 +0200

    Create color.cpp

---
+++ color.cpp
@@ -0,0 +1,17 @@
+/*The purpose of this file is to transform any type of color
+* accepted by png, this is to say rgb, grayscale, rgba and palette, in the rgb565 format used by Numworks Calculator.
+*/
+
+template<typename PNGColor>
+
+int _to_rgb565(PNGColor const * const color){
+  uint8_t rgb565_color = 0;
+  if (color == 3){
+    //Format is rgb888
+    
+  }
+}
+
+int main(){
+  return none;
+}
